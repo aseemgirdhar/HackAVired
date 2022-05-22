659dfd9571d65be243100e63be68d2907c44eb6a
aseemgirdhar <aseemgirdhar9@gmail.com>
2022-05-22 11:45:21 +0530

    WIP: Header ui

--- social-vired/src/App.js
+++ social-vired/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from "./components/dashboard/dashboard";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Header />
     </div>
   );
 }
